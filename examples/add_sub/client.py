c7b0336a2af79d568d1a82c06c97a853603d70ca
HongTao Wang <wanhuatong@126.com>
2025-07-23 11:25:49 +0800

    Update client.py

--- examples/add_sub/client.py
+++ examples/add_sub/client.py
@@ -6,7 +6,8 @@ triton_url = "localhost:8000"
 model_name = "openpom_model"
 
 smiles = ["CC(=O)OC1=CC=CC=C1C(=O)O"]
-input_data = np.array(smiles, dtype=object).reshape(1, 1)
+#input_data = np.array(smiles, dtype=object).reshape(1, 1)
+input_data = np.array([s.encode("utf-8") for s in smiles], dtype=np.bytes_).reshape(len(smiles), 1)
 
 with httpclient.InferenceServerClient(triton_url) as client:
     inputs = [
@@ -17,6 +18,11 @@ with httpclient.InferenceServerClient(triton_url) as client:
         )
     ]
     inputs[0].set_data_from_numpy(input_data)
+    print("Input dtype:", input_data.dtype)  # 
+    print("Input shape:", input_data.shape)  #(1, 1)
+    print("Input content:", input_data)
+    print("input_data[0, 0]:", input_data[0, 0], type(input_data[0, 0]))
+
 
     outputs = [
         httpclient.InferRequestedOutput("OUTPUT", binary_data=True)
